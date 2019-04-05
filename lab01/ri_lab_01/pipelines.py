e70def26a31f5e8f0ac7da9e743bf344c7bf7a84
docinho <livia.juliao@ccc.ufcg.edu.br>
2019-04-05 20:24:37 -0300

    lab 1 vazio

---
+++ lab01/ri_lab_01/pipelines.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+from scrapy.conf import settings
+
+from ri_lab_01.items import RiLab01Item
+from ri_lab_01.items import RiLab01CommentItem
+
+class RiLab01Pipeline(object):
+    def process_item(self, item, spider):
+        return item
